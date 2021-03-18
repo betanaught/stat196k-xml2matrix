f1d8ba37a9feb63df6363dc39256f1b1266919a3
Brendan Wakefield <22670871+betanot@users.noreply.github.com>
2021-03-17 21:32:48 -0700

    update psuedocode; extract_data() and file_list() work

--- xml2matrix.jl
+++ xml2matrix.jl
@@ -1,6 +1,7 @@
 using Debugger
 using EzXML
 using TextAnalysis
+using DataFrames
 
 f = "file1.xml"
 f = "file2.xml"
@@ -17,11 +18,11 @@ mission_desc = findall("//MissionDesc", root(doc))
 [nodecontent(i) for i in findall("//MissionDescription", root(doc))] # Empty!!
 
 org_desc = findfirst("//Desc/text()", doc)
-nodecontent(org_desc)
+# nodecontent(org_desc)
 if isnothing(org_desc)
     org_desc = findfirst("//Description/text()", doc)
 end
-nodecontent(org_desc)
+# nodecontent(org_desc)
 
 
 ### 3/15 1:04:00
@@ -31,13 +32,39 @@ nodecontent(org_desc)
 ### 1:07:00 for extracting elements into array of strings for corpus creation)
 
 
-function getcontent(xml_file)
+function extract_text(xml_file)
     doc = EzXML.readxml(xml_file)
     org_desc = findfirst("//Desc/text()", doc)
     if isnothing(org_desc)
         org_desc = findfirst("//Description/text()", doc)
     end
+    return org_desc
 end
 
-map(getcontent, files)
+map(extract_text, file_list)
     
+## PSEUDOCODE -------------------------
+# Read in XML file names
+file_list = readdir("./data")
+
+for file in file_list
+    doc = extract_text(file)
+    StringDocument(doc)
+    tokens(doc)
+    prepare!(doc, strip_punctuation)
+    remove_case!(doc)
+    stem!(doc)
+end
+
+# Extract text
+function extract_text(xml_file)
+    doc = EzXML.readxml(xml_file)
+    org_name = findfirst("//Name/text()", doc)
+    org_size = readxml()
+    org_doc = readxml()
+end
+
+# Process text
+
+create_dtm()
+update_corpus()
