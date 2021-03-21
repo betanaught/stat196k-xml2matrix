e7c244685132c950a453992cfc2dfb950bd137d2
Brendan Wakefield <22670871+betanot@users.noreply.github.com>
2021-03-21 16:50:43 -0700

    Add extract_name() and extract_size()

--- xml2matrix.jl
+++ xml2matrix.jl
@@ -3,7 +3,23 @@ using EzXML
 using TextAnalysis
 using DataFrames
 
-function extract_text(xml_file)
+function extract_name(xml_file)
+    xml_string = EzXML.readxml(xml_file)
+    org_name = nodecontent(findfirst("//Filer
+                                      //BusinessName
+                                      //BusinessNameLine1Txt/text()",
+                                      xml_string))
+    return org_name
+end
+
+function extract_size(xml_file)
+    xml_string = EzXML.readxml(xml_file)
+    org_size = nodecontent(findfirst("//TotalEmployeeCnt/text()", xml_string))
+    return org_size
+end
+
+function extract_desc(xml_file)
+    processed_file_count = 0
     xml_string = EzXML.readxml(xml_file)
     org_desc = findfirst("//Desc/text()", xml_string)
     if isnothing(org_desc)
@@ -14,13 +30,16 @@ function extract_text(xml_file)
     remove_case!(doc)
     stem!(doc)
     text(doc)
+    
+    processed_file_count += 1
+    print(string("Files successfully processed: ", processed_file_count))
     return doc
 end
 
 data_dir = "./data/"
 file_list = [data_dir * i for i in readdir(data_dir)]
 
-xml_corpus = Corpus(map(extract_text, file_list))
+xml_corpus = Corpus(map(extract_desc, file_list))
 update_lexicon!(xml_corpus)
 xlm_dtm = DocumentTermMatrix(xml_corpus)
 
