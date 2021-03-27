53fcc283879694f1687fd2f0c1177280b40be48f
Brendan Wakefield <22670871+betanot@users.noreply.github.com>
2021-03-27 15:30:28 -0700

    refactor code: individual functions extract_name() extract_revenue() extract_desc() generate_xml_dict()

--- xml2matrix.jl
+++ xml2matrix.jl
@@ -4,35 +4,74 @@ using Serialization
 
 function extract_name(xml_file)
     xml_string = EzXML.readxml(xml_file)
-    org_name = nodecontent(findfirst("//Filer
-                                      //BusinessName
-                                      //BusinessNameLine1Txt/text()",
-                                      xml_string))
+    test_string = findfirst("//Filer
+                             //BusinessName
+                             //BusinessNameLine1Txt/text()", xml_string)
+    if !isnothing(test_string)
+        org_name = nodecontent(test_string)
+    else
+        org_name = nothing
+    end
     return org_name
 end
 
-function extract_size(xml_file)
+function extract_revenue(xml_file)
     xml_string = EzXML.readxml(xml_file)
-    org_size = nodecontent(findfirst("//TotalEmployeeCnt/text()", xml_string))
-    return org_size
+    test_string = findfirst("//ReturnData
+                             //IRS990EZ
+                             //TotalRevenueAmt/text()", xml_string)
+    if isnothing(test_string)
+        test_string = findfirst("//ReturnData
+                                 //IRS990
+                                 //RevenueAmt/text()", xml_string)
+    end
+    if !isnothing(test_string)
+        org_rev = nodecontent(test_string)
+    else
+        org_rev = nothing
+    end
+    return org_rev
 end
 
 function extract_desc(xml_file)
     xml_string = EzXML.readxml(xml_file)
-    org_desc = findfirst("//Desc/text()", xml_string)
-    if isnothing(org_desc)
-        org_desc = findfirst("//Description/text()", xml_string)
+    test_string = findfirst("//Desc/text()", xml_string)
+    if isnothing(test_string)
+        test_string = findfirst("//Descriptions/text()", xml_string)
+    elseif isnothing(test_string)
+        test_string = findfirst("//ActivityOrMissionDesc")
+    elseif isnothing(test_string)
+        test_string = findfirst("//PrimaryExemptPurposeTxt/text()")
+    elseif isnothing(test_string)
+        test_string = findfirst("//ExplanationTxt/text()")
+    elseif isnothing(test_string)
+        test_string = findfirst("//DescriptionProgramSrvcAccomTxt/text()")
     end
-    doc = StringDocument(nodecontent(org_desc))
-    prepare!(doc, strip_punctuation)
-    remove_case!(doc)
-    stem!(doc)
-    text(doc)
-    
-    return doc
+    if !isnothing(test_string)
+        org_desc = nodecontent(test_string)
+    else
+        org_desc = nothing
+    end
+        # doc = StringDocument(nodecontent(org_desc))
+        # prepare!(doc, strip_punctuation)
+        # remove_case!(doc)
+        # stem!(doc)
+        # text(doc)
+        # return doc
+    # end
 end
 
-function main()
+function generate_xml_dict(file)
+    Dict("file"     => file,
+         "org_name" => extract_name(file),
+         "org_rev"  => extract_revenue(file),
+         "org_desc" => extract_desc(file))
+    end
+end
+
+dict_array = map(generate_xml_dict, readdir())
+
+function main(data_dir = "./data/")
     data_dir = "./data/"
     file_list = [data_dir * i for i in readdir(data_dir)]
 
@@ -48,8 +87,17 @@ function main()
 
     println(string("Total Dictionary entries: ", length(xml_dict)))
     println(string("Total descriptions extracted: ", length(xml_corpus)))
-    serialize("xml_dtm.jld", xml_dtm)
-    serialize("xml_dict.jld", xml_dict)
+    serialize("../xml_dtm.jld", xml_dtm)
+    serialize("../xml_dict.jld", xml_dict)
 end
 
-main()
+if abspath(PROGRAM_FILE) == @__FILE__
+    main()
+end
+
+## SINGLE FILE TESTING ---------------------------------------------------------
+test_file = readdir()[1]
+xml_dict = Dict("file" => test_file,
+                "org_name" => extract_name(test_file),
+                "org_rev"  => extract_revenue(test_file),
+                "org_desc" => extract_desc(test_file))
